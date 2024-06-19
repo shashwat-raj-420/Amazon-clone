16ec1f19d51c13a90cf20e2d99eb2d0a71aa2e7d
shashwat-raj-420 <119058489+shashwat-raj-420@users.noreply.github.com>
2024-06-19 14:52:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function searchMovies(search_input) {
+    const searchTerm = document.getElementById("search-input").value;
+    const selectedGenre = document.getElementById("genre-filter").value;
+}
